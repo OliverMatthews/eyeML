4c6f0641ce1999e57237e29ad84a2fb47824a932
Oliver Pelling-Matthews <oli.matthews@uea.ac.uk>
2023-01-31 21:53:15 +0000

    Moved STARE data exploration to jupyter notebook

--- Main.py
+++ Main.py
@@ -1,15 +1,5 @@
-import pandas as pd
 import scripts.STARE as STARE
 
 
 # Download the STARE dataset
 STARE.download_complete()
-
-# Load the STARE dataset from the image_diagnoses.csv file
-data = pd.read_csv("./data/STARE/diagnoses.csv")
-
-# Print the first 5 rows of the dataset
-print(data.head())
-
-# Print the number of rows and columns in the dataset
-print(data.shape)
